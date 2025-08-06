0e90abafe12289f87a08e89e3ac5c974f3b0baf6
Kolziak <florysiakkamil@gmail.com>
2025-08-07 00:02:53 +0200

    fixed sustain chars animations

--- songs/sustainAnim.hx
+++ songs/sustainAnim.hx
@@ -1,28 +1,22 @@
-var firstSusMissed:Bool = true;
-
 function onNoteHit(e){
     if(e.note.isSustainNote){
         e.preventAnim();
+        e.character.lastAnimContext = 'LOCK';
         e.character.danceOnBeat = false;
+        if(!e.note.nextNote.isSustainNote)
+            e.character.lastAnimContext = 'SING';
     }
 }
 
 function onPlayerMiss(e){
     if(e.note.isSustainNote){
-        if(firstSusMissed){
-            firstSusMissed = false;
-        } else{
-            if(!e.character.animation.curAnim.paused){
-                e.character.animation.curAnim.pause();
-                e.preventAnim();
-            }
-            e.forceAnim = false;
-            e.preventVocalsUnmute();
-            e.healthGain = e.misses = e.score = e.accuracy = 0;
-            e.preventMissSound();
+        e.character.lastAnimContext = 'LOCK';
+        e.character.danceOnBeat = false;
+        if(!e.note.nextNote.isSustainNote)
+            e.character.lastAnimContext = 'MISS';
+        e.preventAnim();
+        e.preventVocalsUnmute();
+        e.healthGain = e.misses = e.score = e.accuracy = 0;
+        e.preventMissSound();
         }
-    } else {
-        if(e.note.nextNote.isSustainNote)
-            firstSusMissed = false;
-    }
 }
