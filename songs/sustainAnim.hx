5bc2b840f25f46f33bb52c513e2307123b3865bd
Kolziak <florysiakkamil@gmail.com>
2025-08-07 15:00:32 +0200

    south erect + one last sustains fix 😭

--- songs/sustainAnim.hx
+++ songs/sustainAnim.hx
@@ -2,20 +2,28 @@ function onNoteHit(e){
     if(e.note.isSustainNote){
         e.preventAnim();
         e.character.lastAnimContext = 'LOCK';
-        if(e.note.nextNote == null || !e.note.nextNote.isSustainNote)
-            e.character.lastAnimContext = 'SING';
+        if(e.note.nextNote == null || !e.note.nextNote.isSustainNote){
+            var event = e;
+            new FlxTimer().start(0.1, function(timer) {
+                event.character.lastAnimContext = 'SING';
+            });
+        }
     }
 }
 
 function onPlayerMiss(e){
     if(e.note.isSustainNote){
+        var e = e;
         e.character.lastAnimContext = 'LOCK';
-        trace(e.note.nextNote);
-        if(e.note.nextNote == null || !e.note.nextNote.isSustainNote)
-            e.character.lastAnimContext = 'MISS';
+        if(e.note.nextNote == null || !e.note.nextNote.isSustainNote){
+            var event = e;
+            new FlxTimer().start(0.1, function(timer) {
+                event.character.lastAnimContext = 'MISS';
+            });
+        }
         e.preventAnim();
         e.preventVocalsUnmute();
         e.healthGain = e.misses = e.score = e.accuracy = 0;
         e.preventMissSound();
-        }
+    }
 }
