03b0fe7e38b8d282a555f2ccee334e4ee697656a
Ralty <78720179+Raltyro@users.noreply.github.com>
2025-08-08 00:25:16 +0700

    Test

--- songs/nightmareInst.hx
+++
@@ -1,6 +0,0 @@
-var game = PlayState.instance;
-
-function onSongStart() {
-    game.inst = FlxG.sound.load(Assets.getMusic(Paths.inst(game.SONG.meta.name, game.difficulty == 'nightmare' ? 'erect' : game.difficulty)));
-    FlxG.sound.music = game.inst;
-}
