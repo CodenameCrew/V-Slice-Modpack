058d9cd654691f4b3aa570896a8d403b4b0f81aa
Ralty <78720179+Raltyro@users.noreply.github.com>
2025-08-07 19:59:16 +0700

    gif gfComboAnims

--- songs/gfComboAnims.hx
+++ songs/gfComboAnims.hx
@@ -1,11 +1,17 @@
-function onPlayerHit(e){
-    if(combo == 49)
-        gf.playAnim('combo50', true);
-    if(combo == 199)
-        gf.playAnim('combo200', true);
+function onPostNoteHit(e) {
+	if (combo == 50) {
+		if (gf.hasAnim('combo50')) gf.playAnim('combo50', true);
+		else if (gf.hasAnim('cheer')) gf.playAnim('cheer', true);
+	}
+	else if (combo == 200) {
+		if (gf.hasAnim('combo200')) gf.playAnim('combo200', true);
+		else if (gf.hasAnim('cheer')) gf.playAnim('cheer', true);
+	}
 }
 
-function onPlayerMiss(e){
-    if(combo >= 70)
-        gf.playAnim('drop70', true);
+function onPlayerMiss(e) {
+	if (combo >= 70) {
+		if (gf.hasAnim('drop70')) gf.playAnim('drop70', true);
+		else if (gf.hasAnim('sad')) gf.playAnim('sad', true);
+	}
 }
