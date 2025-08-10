f605dc4a99ff142b47f26101560f43d89a927359
Ralty <78720179+Raltyro@users.noreply.github.com>
2025-08-10 23:21:53 +0700

    Pixel Shader shall zoom

--- songs/vslice_uiShits.hx
+++ songs/vslice_uiShits.hx
@@ -1,5 +1,3 @@
-public var ratingScaleDiff:Float = 0.1;
-
 var lerpHealth:Float = 1;
 
 function postCreate() {
@@ -8,12 +6,11 @@ function postCreate() {
 	comboGroup.cameras = [camHUD];
 }
 
+function onCountdown(e) if (e.scale == 0.6) e.scale = 1;
+
 function onPostCountdown(e) {
 	var spr = e.sprite;
-	if (spr != null) {
-		spr.camera = camHUD;
-		spr.scale.set(1, 1);
-	}
+	if (spr != null) spr.camera = camHUD;
 
 	// prevents tweening the y  - Nex
 	var props = e.spriteTween?._propertyInfos;
@@ -21,11 +18,11 @@ function onPostCountdown(e) {
 		if (info.field == "y") e.spriteTween._propertyInfos.remove(info);
 }
 
-function onNoteHit(e) {
+function onNoteHit(e) if (e.ratingPrefix == 'game/score/') {
 	e.numScale *= 0.9;
 	e.ratingScale *= 0.9;
 }
-function onPostNoteHit(e) comboGroup.cameras = [camHUD];
+//function onPostNoteHit(e) comboGroup.cameras = [camHUD];
 
 function postUpdate(elapsed:Float) {
 	lerpHealth = lerp(lerpHealth, health, 0.15);
