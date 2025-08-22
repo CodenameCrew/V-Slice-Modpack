b12b7db327d5a76cd10308a0a15a05c487f82d82
Kolziak <florysiakkamil@gmail.com>
2025-08-22 22:39:55 +0200

    fixed another sustain anim issue

--- songs/sustainAnim.hx
+++ songs/sustainAnim.hx
@@ -17,11 +17,8 @@ function onPlayerMiss(e){
 	if (e.note?.isSustainNote){
 		for(i in 0...e.characters.length){
 			e.characters[i].lastAnimContext = 'LOCK';
-			if(e.note.nextNote == null || !e.note.nextNote.isSustainNote){
-				var event = e;
-				event.characters[i].lastHit = Conductor.songPosition + 30;
-				event.characters[i].lastAnimContext = 'MISS';
-			}
+			if(e.note.nextNote == null || !e.note.nextNote.isSustainNote)
+				e.characters[i].lastAnimContext = 'MISS';
 		}
 		e.preventAnim();
 		e.preventVocalsUnmute();
