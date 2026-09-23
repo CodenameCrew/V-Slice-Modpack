0e493787b45df94cb05ad0938fa2b7b5962bca44
Kolze <53307233+KolzeYT@users.noreply.github.com>
2026-09-23 21:55:56 +0200

    oops forgot to change that back

--- data/characters/darnell.hx
+++ data/characters/darnell.hx
@@ -52,7 +52,7 @@ function loadCanSprite() {
 	canSprite.loadSprite(Paths.image("characters/spraycanAtlas"));
 	canSprite.addAnim('Can Start', 'Can with Labels', 24, false, false, [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]);
 	canSprite.addAnim('Hit Pico',  'Can with Labels', 24, false, false, [19,20,21,22,23,24,25]);
-	canSprite.addAnim('Can Shot',  'Can with Labels', 24, false, false, [26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50]);
+	canSprite.addAnim('Can Shot',  'Can with Labels', 24, false, false, [26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42]);
 	canSprite.visible = false;
 	FlxG.state.insert(game.members.indexOf(this), canSprite);
 }
